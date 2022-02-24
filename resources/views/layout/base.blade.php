2daf869674dea975b530588b570c3aacc3ca3169
ardhptr21 <ardhiputrapradana21@gmail.com>
2022-02-24 20:31:53 +0700

    change to use path instead of asset method to serve static file

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@isset($title){{ $title }} @else Antrian App @endisset</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body class="font-sans antialiased">
@@ -14,7 +14,7 @@
         @yield('content')
     </main>
 
-    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="/js/script.js"></script>
 </body>
 
 </html>
